6419487443652bce397f6096fd2137a0db90fac5
OmarRashdan <81979296+OmarRashdan@users.noreply.github.com>
2022-04-29 12:06:46 +0200

    Change made in the starting point of the IMFs
    As brown dwarfs are too faint because of their low mass, we have made the decision to change the starting point of all of the IMFs to start from 0.1; some were originally designed, by the authors, to start from 0.01. By changing this, I had to normalize the IMFs that were change again, where the normalization constants changed; these change were implemented

--- model/cluster_distribution.py
+++ model/cluster_distribution.py
@@ -98,7 +98,7 @@ class Mod_MyFunctions:
             y=[]#np.zeros(len(x))    
             for i in x:
                 if (i > np.log10(k0)) and (i <= np.log10(k1)):
-                   y.append(A1*(10.0**i)**(-x1))
+                    y.append(A1*(10.0**i)**(-x1))
                 if (i > np.log10(k1)) and (i <= np.log10(k2)):
                     y.append(A2*(10.0**i)**(-x2))
                 if (i > np.log10(k2)) and (i <= np.log10(k3)):
@@ -112,20 +112,22 @@ class Mod_MyFunctions:
             #Arr4B = np.concatenate((a1,a2,a3,a4))
             return y
 
-        # Chabrier 2001, from 0.01 to 100
+        # Chabrier 2001, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 3:
             
-            A1Chabrier01 = 0.376613899390368
+            A1Chabrier01 = 0.678017843874733
             
             return A1Chabrier01 * 40.33 * (10.0**x)**(-3.3) * np.exp(- (716.4/(10.0**x))**(0.25) )
         
-        # Chabrier 2005, from 0.01 to 100
+        # Chabrier 2005, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 4:
             
-            A1Chabrier05 = 0.9652005
+            A1Chabrier05 = 0.59906415 # 
             mcChabrier05 = 0.2
             sigmaChabrier05 = 0.55
-            A2Chabrier05 = 0.4255185
+            A2Chabrier05 = 0.26410355 # 0.041*6.44155
             x0Chabrier05 = 2.35
 
             a1 = A1Chabrier05 * np.exp(-((x-np.log10(mcChabrier05))**2)/(2*sigmaChabrier05**2))
@@ -133,13 +135,14 @@ class Mod_MyFunctions:
             
             return np.where(x <= np.log10(mnorm), a1, a2)
         
-        # Chabrier 2003 (but inserted myself and normalized, "standard", just to test), from 0.01 to 100
+        # Chabrier 2003 (but inserted myself and normalized, "standard", just to test), from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 444:
             
-            A1Chabrier03 = 1.2999
+            A1Chabrier03 = 1.44593858 # 0.158*9.15151
             mcChabrier03 = 0.079
             sigmaChabrier03 = 0.69
-            A2Chabrier03 = 0.359
+            A2Chabrier03 = 0.405411893 # 0.0443*9.15151
             x0Chabrier03 = 2.3
 
             a1 = A1Chabrier03 * np.exp(-((x-np.log10(mcChabrier03))**2)/(2*sigmaChabrier03**2))
@@ -157,7 +160,7 @@ class Mod_MyFunctions:
             a2 = A2Caonical * (10.0**x)**(-2.35)
             return np.where(x <= np.log10(0.5), a1, a2)
         
-        # Salpeter 1955, Bottom-heavy, from 0.01 to 100 (originally defied from exp(-0.4) to exp(1))
+        # Salpeter 1955, Bottom-heavy, from 0.1 to 100 (originally defied from exp(-0.4) to exp(1))
         if imf_type == 6:
 
             A1Salpeter = 0.926709878283741
@@ -293,112 +296,104 @@ class Mod_MyFunctions:
 
             return a1
 
-        # Kroupa 2001, Universal, from 0.01 to 100
+        # Kroupa 2001, Universal, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 20:
             
-            A1Kroupa01 = 0.350911129985083
-            A2Kroupa01 = 0.158972099575776
-            A3Kroupa01 = 0.0794860497878882
+            A1Kroupa01 = 0.286276391369702
+            A2Kroupa01 = 0.143138195684851
 
-            Rezultz = ThreeBreakIMF(x,A1=A1Kroupa01,A2=A2Kroupa01,A3=A3Kroupa01,x1=0.3,x2=1.3,x3=2.3,k0=0.01,k1=0.08,k2=0.5,k3=100)
+            a1 = A1Kroupa01 * (10.0**x)**(-1.3)
+            a2 = A2Kroupa01 * (10.0**x)**(-2.3)
 
-            return Rezultz
+            #Rezultz = ThreeBreakIMF(x,A1=A1Kroupa01,A2=A2Kroupa01,A3=A3Kroupa01,x1=0.3,x2=1.3,x3=2.3,k0=0.01,k1=0.08,k2=0.5,k3=100)
+
+            return np.where(x <= np.log10(0.5), a1, a2)
         
-        # Modified Kroupa01 1 (form Meurer et al. 2009), Top-light, from 0.01 to 100
+        # Modified Kroupa01 1 (form Meurer et al. 2009), Top-light, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 21:
             
-            A1mod1Kroupa01 = 2.12598723577527
-            A2mod1Kroupa01 = 0.170078978862021
-            A3mod1Kroupa01 = 0.0425197447155053
+            A1mod1Kroupa01 = 0.324429145997708
+            A2mod1Kroupa01 = 0.0811072864994270
 
-            Rezultz = ThreeBreakIMF(x,A1=A1mod1Kroupa01,A2=A2mod1Kroupa01,A3=A3mod1Kroupa01,x1=0.3,x2=1.3,x3=3.3,k0=0.01,k1=0.08,k2=0.5,k3=100)
+            a1 = A1mod1Kroupa01 * (10.0**x)**(-1.3)
+            a2 = A2mod1Kroupa01 * (10.0**x)**(-3.3)
 
-            return Rezultz
+            #Rezultz = ThreeBreakIMF(x,A1=A1mod1Kroupa01,A2=A2mod1Kroupa01,A3=A3mod1Kroupa01,x1=0.3,x2=1.3,x3=3.3,k0=0.01,k1=0.08,k2=0.5,k3=100)
 
-        # Modified Kroupa01 2 (form Meurer et al. 2009), Top-heavy, from 0.01 to 100
+            return np.where(x <= np.log10(0.5), a1, a2)
+
+        # Modified Kroupa01 2 (form Meurer et al. 2009), Top-heavy, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 22:
             
-            A1mod2Kroupa01 = 1.45165470227029
-            A2mod2Kroupa01 = 0.116132376181623
+            A1mod2Kroupa01 = 0.172011082357144
 
-            a1 = A1mod2Kroupa01 * (10.0**x)**(-0.3)
-            a2 = A2mod2Kroupa01 * (10.0**x)**(-1.3)
+            a1 = A1mod2Kroupa01 * (10.0**x)**(-1.3)
 
-            return np.where(x <= np.log10(0.08), a1, a2)
+            return a1
 
-
-        # Modified Kroupa01 2 (form Meurer et al. 2009), Top-heavy, from 0.01 to 100
-        if imf_type == 220:
-            
-            A1mod2Kroupa01 = 1.45165470227029
-            A2mod2Kroupa01 = 0.116132376181623
-
-            #a1 = A1mod2Kroupa01 * (10.0**x)**(-0.3)
-            #a2 = A2mod2Kroupa01 * (10.0**x)**(-1.3)
-
-            Rezultz = ThreeBreakIMF(x,A1=A1mod2Kroupa01,A2=A2mod2Kroupa01,A3=A2mod2Kroupa01,x1=0.3,x2=1.3,x3=1.3,k0=0.01,k1=0.08,k2=0.5,k3=100)
-
-            return Rezultz
-
-        # Modified Kroupa01 3 (form Wilkins et al. 2008b), Universal, from 0.01 to 100
+        # Modified Kroupa01 3 (form Wilkins et al. 2008b), Universal, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 23:
             
-            A1mod3Kroupa01 = 1.99821313724425
-            A2mod3Kroupa01 = 0.159857050979540
-            A3mod3Kroupa01 = 0.0772058664879645
+            A1mod3Kroupa01 = 0.289159021998465
+            A2mod3Kroupa01 = 0.139654602092348
 
-            Rezultz = ThreeBreakIMF(x,A1=A1mod3Kroupa01,A2=A2mod3Kroupa01,A3=A3mod3Kroupa01,x1=0.3,x2=1.3,x3=2.35,k0=0.01,k1=0.08,k2=0.5,k3=100)
+            a1 = A1mod3Kroupa01 * (10.0**x)**(-1.3)
+            a2 = A2mod3Kroupa01 * (10.0**x)**(-2.35)
 
-            return Rezultz
+            #Rezultz = ThreeBreakIMF(x,A1=A1mod3Kroupa01,A2=A2mod3Kroupa01,A3=A3mod3Kroupa01,x1=0.3,x2=1.3,x3=2.35,k0=0.01,k1=0.08,k2=0.5,k3=100)
 
-        # van Dokkum 2008, Bottom-light, from 0.01 to 100
+            return np.where(x <= np.log10(0.5), a1, a2)
+
+        # van Dokkum 2008, Bottom-light, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 24:
-            
-            #A1Chabrier05 = 0.9652005
-            #mcChabrier05 = 0.2
-            #sigmaChabrier05 = 0.55
-            #A2Chabrier05 = 0.4255185
-            #x0Chabrier05 = 2.35
-
-            A1VD08             = 0.511
+            A1VD08             = 0.51043004 # (0.140-0.027)*4.51708
             mcVD08             = 0.079 # 0.079, 0.4, 2
             sigVD08            = 0.69 # 0.69
-            AhVD08             = 0.32
+            AhVD08             = 0.322067804# (0.0443+0.027)*4.51708
             xVD08              = 2.3
             ncVD08             = 25
 
             a1 = A1VD08*(0.5*ncVD08*mcVD08)**(-xVD08) * np.exp(-((x - np.log10(mcVD08))**2)/(2*sigVD08**2))
             a2 = AhVD08 * (10.0**x)**(-xVD08)
             
-            return np.where(x <= np.log10(ncVD08*mcVD08), a1, a2)
+            return np.where(x <= np.log10(ncVD08*mcVD08), a1, a2) # ncVD08*mcVD08 = 1.975
 
-        # Larson 1998 (from Portinari 2004), Bottom-light, from 0.01 to 100
+        # Larson 1998 (from Portinari 2004), Bottom-light, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 25:
             
-            A1Larson04 = 0.817193205330865
+            A1Larson04 = 0.873129716700659
             
             return A1Larson04 * 0.317 * (10.0**x)**(-2.35) * np.exp(- 0.3375/(10.0**x))
 
-        # Modified Larson (from Portinari 2004), Bottom-light, from 0.01 to 100
+        # Modified Larson (from Portinari 2004), Bottom-light, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 26:
             
-            A1Larson04 = 0.592524862490177
+            A1Larson04 = 0.623676664314137
             
             return A1Larson04 * 0.4337 * (10.0**x)**(-2.7) * np.exp(- 0.425/(10.0**x))
 
-        # Cappellari et al. BH 2012, Bottom-heavy, from 0.01 to 100
+        # Cappellari et al. BH 2012, Bottom-heavy, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 27:
             
-            A1BHCappellari12 = 0.000452139586199803
+            A1BHCappellari12 = 0.0285281910370742
 
             a1 = A1BHCappellari12 * (10.0**x)**(-2.8)
 
             return a1
 
-        # Cappellari et al. TH 2012, Top-heavy, from 0.01 to 100
+        # Cappellari et al. TH 2012, Top-heavy, from 0.1 to 100
+        #change made in the starting point of the IMF; brown dwarfs are too faint
         if imf_type == 28:
             
-            A1THCappellari12 = 0.0505050505050505
+            A1THCappellari12 = 0.163277160168587
 
             a1 = A1THCappellari12 * (10.0**x)**(-1.5)
 
@@ -424,6 +419,7 @@ class Mod_MyFunctions:
 
             return Rezultz
         
+        # Tester
         if imf_type == 30:
             
             return (10.0**x)**(-1) #np.where(x<= np.log10(1), (10.0**x)**1, (10.0**x)**2)
@@ -533,7 +529,7 @@ class Mod_MassRad:
         output=1,
         FILE_dist = dist_file):
 
-        m_temp = myf.mass_dist(mmin = 0.01, mmax = 100., Mcm = Mcm, imf_type = imf_type, SFE=SFE)
+        m_temp = myf.mass_dist(mmin = 0.1, mmax = 100., Mcm = Mcm, imf_type = imf_type, SFE=SFE) #change made in the starting point of the IMFs; brown dwarfs are too faint
         N=len(m_temp)
 
         self.m = m_temp
